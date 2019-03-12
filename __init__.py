58dd1bf1d931368cec82961188a5d763ac8d8864
K4YT3X <k4yt3x@k4yt3x.com>
2019-03-12 11:50:53 -0400

    1.6.3 added batch option

--- __init__.py
+++ __init__.py
@@ -25,15 +25,26 @@ if sys.platform == 'win32':
 else:
     import syslog
 
-VERSION = '1.6.2'
+VERSION = '1.6.3'
 
 
 class Avalon:
+    """ Avalon Standard Input/Output Framework
+
+    The avalon framework provides convenient and beautiful methods
+    to handle command line user input or program output. It's based on
+    Unix console colors, and is made compatible with the Windows platform
+    with the colorama library.
+    """
 
     class FG():
+        """ Foreground Colors
+
+        This class contains all foreground colors.
+        Access colors via Avalon.FG.Color.
         """
-            Foreground Color
-        """
+
+        # Standard colors
         BL = '\033[30m'  # Black
         R = '\033[31m'  # Red
         G = '\033[32m'  # Green
@@ -41,6 +52,8 @@ class Avalon:
         B = '\033[34m'  # Blue
         M = '\033[35m'  # Magenta
         C = '\033[36m'  # Cyan
+        
+        # Light colors
         LGR = '\033[37m'  # Light Grey
         DGR = '\033[90m'  # Dark Grey
         LR = '\033[91m'  # Light Red
@@ -52,8 +65,10 @@ class Avalon:
         W = '\033[97m'  # White
 
     class BG():
-        """
-            Background Color
+        """ Foreground Colors
+
+        This class contains all background colors.
+        Access colors via Avalon.BG.Colors.
         """
         BL = '\033[40m'  # Black
         R = '\033[41m'  # Red
@@ -73,8 +88,11 @@ class Avalon:
         WT = '\033[107m'  # White
 
     class FM():
-        """
-            Formatting
+        """ Formatting Sequences
+
+        This class contains all formatting-related
+        sequences, such as bold or italic.
+        Access formats via Avalon.FM.Color.
         """
         # SET
         BD = '\033[1m'  # Bold
@@ -94,49 +112,73 @@ class Avalon:
         RHD = '\033[28m'  # Hidden
 
     def info(msg, log=False):
+        """ print regular information
+        """
         print('{}[+] INFO: {}{}'.format(Avalon.FG.G, str(msg), Avalon.FM.RST))
         if log and sys.platform != 'win32':
             syslog.syslog(syslog.LOG_INFO, msg)
 
     def time_info(msg, log=False):
+        """ print regular information with time stamp
+        """
         import datetime
         print('{}{}{} [+] INFO: {}{}'.format(Avalon.FM.RST, str(datetime.datetime.now()), Avalon.FG.G, str(msg), Avalon.FM.RST))
         if log and sys.platform != 'win32':
             syslog.syslog(syslog.LOG_INFO, msg)
 
     def debug_info(msg, log=True):
+        """ print information fo debugging
+        """
         import datetime
         print('{}{} [+] INFO: {}{}'.format(Avalon.FG.DGR, str(datetime.datetime.now()), str(msg), Avalon.FM.RST), file=sys.stderr)
         if log and sys.platform != 'win32':
             syslog.syslog(syslog.LOG_DEBUG, msg)
 
     def warning(msg, log=False):
+        """ print a warning message
+        """
         print('{}{}[!] WARNING: {}{}'.format(Avalon.FG.Y, Avalon.FM.BD, str(msg), Avalon.FM.RST), file=sys.stderr)
         if log and sys.platform != 'win32':
             syslog.syslog(syslog.LOG_WARNING, msg)
 
     def error(msg, log=True):
+        """ print an error message
+        """
         print('{}{}[!] ERROR: {}{}'.format(Avalon.FG.R, Avalon.FM.BD, str(msg), Avalon.FM.RST), file=sys.stderr)
         if log and sys.platform != 'win32':
             syslog.syslog(syslog.LOG_WARNING, msg)
 
     def debug(msg, log=True):
-        print('{}{}[*] DBG: {}{}'.format(Avalon.FG.R, Avalon.FM.RDM, str(msg), Avalon.FM.RST), file=sys.stderr)
+        """ print a debug message
+        """
+        print('{}{}[*] DEBUG: {}{}'.format(Avalon.FG.R, Avalon.FM.RDM, str(msg), Avalon.FM.RST), file=sys.stderr)
         if log and sys.platform != 'win32':
             syslog.syslog(syslog.LOG_DEBUG, msg)
 
-    def gets(msg):
+    def gets(msg, default=None, batch=False):
+        """ Gets user input as a string
+        """
+
+        # if batch is set, return the default value
+        if batch:
+            return default
+
         print('{}{}[?] USER: {}{}'.format(Avalon.FG.Y, Avalon.FM.BD, msg, Avalon.FM.RST), end='')
         return input()
 
-    def ask(msg, default=False):
+    def ask(msg, default=False, batch=False):
         """ Gets a True / False answer from user
 
         This method will ask user a question that will
         require a true / false answer. Pressing enter without
         entering anything will return the default value.
         """
-        if default is False:
+
+        # if batch is set, return the default value
+        if batch:
+            return default
+
+        elif default is False:
             while True:
                 ans = Avalon.gets(msg + ' [y/N]: ')
                 if ans == '' or ans[0].upper() == 'N':
